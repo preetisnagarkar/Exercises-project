7eac6d5887aebe0fab44d400b4f3b90203455c0f
preetisnagarkar <34410298+preetisnagarkar@users.noreply.github.com>
2018-04-03 20:17:45 +0530

    add new projects

---
+++ Class.py
@@ -0,0 +1,22 @@
+class Student:
+
+	def  __information__(self, name, grade, age):
+	    self.name = name
+	    self.grade = grade
+	    self.age = age
+
+
+
+
+kitty = Student('kitty', 85, 15)
+deniel = Student('deniel', 70, 25)
+
+print("Name1: ", kitty.name)
+print("Name2: ", deniel.name)
+
+print("Grade1: ", kitty.grade)
+print("Grade2: ", deniel.grade)
+
+print("Age1: ", kitty.age)
+print("Age2: ", deniel.age)
+
